6253461f6cc557a33ea0f2811ee4d351a4dd6733
Reazul-Islam-Fahim <jkabbo@yahoo.com>
2025-06-30 18:58:38 +0600

    medium

--- problem-07.py
+++ problem-07.py
@@ -1,17 +1,34 @@
+#takes 49ms 
+
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         INT_MIN, INT_MAX = -2**31, 2**31 - 1
+#         res = 0
+#         sign = -1 if x < 0 else 1
+#         x = abs(x)
+
+#         while x != 0:
+#             digit = x % 10
+#             x //= 10
+
+#             if res > (INT_MAX - digit) // 10:
+#                 return 0
+
+#             res = res * 10 + digit
+
+#         return sign * res
+
+
+#takes 37ms
+
 class Solution:
     def reverse(self, x: int) -> int:
         INT_MIN, INT_MAX = -2**31, 2**31 - 1
-        res = 0
+        
         sign = -1 if x < 0 else 1
-        x = abs(x)
-
-        while x != 0:
-            digit = x % 10
-            x //= 10
-
-            if res > (INT_MAX - digit) // 10:
-                return 0
-
-            res = res * 10 + digit
-
-        return sign * res
+        reversed_str = str(abs(x))[::-1]
+        res = sign * int(reversed_str)
+        
+        if res < INT_MIN or res > INT_MAX:
+            return 0
+        return res
