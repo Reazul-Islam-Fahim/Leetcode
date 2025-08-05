e14115de1c0ddf4d63ef3634d8c2fb41e457059d
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-08-05 20:23:30 +0600

    easy

--- problem-3477.py
+++ problem-3477.py
@@ -1,19 +1,21 @@
 from typing import List
+from bisect import bisect_left
+from sortedcontainers import SortedList
 
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         n = len(fruits)
-        used = [False] * n 
-        unplaced_count = 0
+        used = [False] * n
+        unplaced = 0
 
-        for i in range(n):
+        for fruit in fruits:
             placed = False
-            for j in range(n):
-                if not used[j] and baskets[j] >= fruits[i]:
-                    used[j] = True
+            for i in range(n):
+                if not used[i] and baskets[i] >= fruit:
+                    used[i] = True
                     placed = True
                     break
             if not placed:
-                unplaced_count += 1
+                unplaced += 1
 
-        return unplaced_count
+        return unplaced
